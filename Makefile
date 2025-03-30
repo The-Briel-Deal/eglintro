ed02075d1068c252bc39e2d13c8968ae7b87b76b
Gabriel Ford <gabe@fordltc.net>
2025-03-30 15:28:03 -0400

    Add wayland-client lib.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS  := -std=gnu99 -Iinclude -Wall -Werror -lEGL
+CFLAGS  := -std=gnu99 -Iinclude -Wall -Werror -lEGL -lwayland-client
 
 include/xdg_shell.h: /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml
 	wayland-scanner client-header $< $@
--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <EGL/eglplatform.h>
 #include <GL/gl.h>
 #include <assert.h>
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <wayland-client-core.h>
 #include <wayland-client-protocol.h>
 #include <wayland-egl-core.h>
 
+#include "xdg_shell.h"
+
 struct wl_display *display = NULL;
 struct wl_compositor *compositor = NULL;
+struct xdg_wm_base *xdg_wm_base = NULL;
 struct wl_surface *surface = NULL;
-struct wl_shell *shell = NULL;
 
 // clang-format off
 EGLint const attrib_list[] = {
@@ -24,24 +24,24 @@ EGLint const attrib_list[] = {
 };
 // clang-format on
 
-static void global_registry_handler(void *data, struct wl_registry *registry,
-                                    uint32_t id, const char *interface,
-                                    uint32_t version) {
-  printf("Got a registry event for %s id %d\n", interface, id);
-  if (strcmp(interface, "wl_compositor") == 0) {
-    compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
-  } else if (strcmp(interface, "wl_shell") == 0) {
-    shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
-  }
-}
-
-static void global_registry_remover(void *data, struct wl_registry *registry,
-                                    uint32_t id) {
-  printf("Got a registry losing event for %d\n", id);
-}
-
-static const struct wl_registry_listener registry_listener = {
-    global_registry_handler, global_registry_remover};
+//static void global_registry_handler(void *data, struct wl_registry *registry,
+//                                    uint32_t id, const char *interface,
+//                                    uint32_t version) {
+//  printf("Got a registry event for %s id %d\n", interface, id);
+//  if (strcmp(interface, "wl_compositor") == 0) {
+//    compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+//  } else if (strcmp(interface, "xdg_wm_base") == 0) {
+//    // shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
+//  }
+//}
+//
+//static void global_registry_remover(void *data, struct wl_registry *registry,
+//                                    uint32_t id) {
+//  printf("Got a registry losing event for %d\n", id);
+//}
+//
+//static const struct wl_registry_listener registry_listener = {
+//    global_registry_handler, global_registry_remover};
 
 int main() {
   EGLDisplay egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
