5049a9a3dec366626602939f6a520301ecf53ebc
Gabriel Ford <gabe@fordltc.net>
2025-04-03 14:34:40 -0400

    Add colors to the different log severities.

--- include/log.h
+++ include/log.h
@@ -3,9 +3,15 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+
+#define ANSI_COLOR_RED    "\x1b[31m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET  "\x1b[0m"
+
 #ifndef LOG_LEVEL
 #define LOG_LEVEL 2
 #endif
+
 
 enum log_severity {
   INFO_LOG  = 0,
@@ -19,13 +25,13 @@ static inline void gf_log(enum log_severity severity, char *fmt_msg, ...) {
   if (severity >= LOG_LEVEL) {
     switch (severity) {
       case INFO_LOG : printf("INFO  - "); break;
-      case DEBUG_LOG: printf("DEBUG - "); break;
-      case ERROR_LOG: printf("ERROR - "); break;
+      case DEBUG_LOG: printf(ANSI_COLOR_YELLOW "DEBUG - "); break;
+      case ERROR_LOG: printf(ANSI_COLOR_RED "ERROR - "); break;
     }
     vprintf(fmt_msg, args);
   }
   va_end(args);
-  printf("\n");
+  printf(ANSI_COLOR_RESET "\n");
 }
 
 #endif
