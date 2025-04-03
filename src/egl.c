859683ca2f6ba7c7be9fabbb5b025b75dd4aba35
Gabriel Ford <gabe@fordltc.net>
2025-04-03 14:09:33 -0400

    Remove eglGetErrorString

--- src/egl.c
+++ src/egl.c
@@ -55,30 +55,6 @@ static const EGLint context_attribs[] = {
     EGL_NONE,
 };
 
-const char *eglGetErrorString(EGLint error) {
-#define CASE_STR(value)                                                        \
-  case value: return #value;
-  switch (error) {
-    CASE_STR(EGL_SUCCESS)
-    CASE_STR(EGL_NOT_INITIALIZED)
-    CASE_STR(EGL_BAD_ACCESS)
-    CASE_STR(EGL_BAD_ALLOC)
-    CASE_STR(EGL_BAD_ATTRIBUTE)
-    CASE_STR(EGL_BAD_CONTEXT)
-    CASE_STR(EGL_BAD_CONFIG)
-    CASE_STR(EGL_BAD_CURRENT_SURFACE)
-    CASE_STR(EGL_BAD_DISPLAY)
-    CASE_STR(EGL_BAD_SURFACE)
-    CASE_STR(EGL_BAD_MATCH)
-    CASE_STR(EGL_BAD_PARAMETER)
-    CASE_STR(EGL_BAD_NATIVE_PIXMAP)
-    CASE_STR(EGL_BAD_NATIVE_WINDOW)
-    CASE_STR(EGL_CONTEXT_LOST)
-    default: return "Unknown";
-  }
-#undef CASE_STR
-}
-
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
                                 GLenum severity, GLsizei length,
                                 const GLchar *message, const void *userParam) {
