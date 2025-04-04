741d2f50f226a7b3407ba3cd95243e2addf1b18b
Gabriel Ford <gabe@fordltc.net>
2025-04-04 15:05:52 -0400

    Removed dependency on cglm ortho function.

--- src/draw.c
+++ src/draw.c
@@ -3,9 +3,9 @@
 #include <GL/glext.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 #include <wayland-util.h>
 
-#include "cglm/cam.h"
 #include "common.h"
 #include "draw.h"
 #include "log.h"
@@ -94,13 +94,40 @@ bool gf_draw_update_window_size(int32_t width, int32_t height) {
   return true;
 }
 
+typedef float vec4[4];
+typedef vec4 mat4[4];
+
+void gf_mat4_zero(mat4 dest) {
+  memset(dest, 0x0, sizeof(mat4));
+  dest = (mat4){0};
+}
+
+void gf_ortho(float left, float right, float bottom, float top, float nearZ,
+              float farZ, mat4 dest) {
+  float rl, tb, fn;
+
+  gf_mat4_zero(dest);
+
+  rl = 1.0f / (right - left);
+  tb = 1.0f / (top - bottom);
+  fn = -1.0f / (farZ - nearZ);
+
+  dest[0][0] = 2.0f * rl;
+  dest[1][1] = 2.0f * tb;
+  dest[2][2] = 2.0f * fn;
+  dest[3][0] = -(right + left) * rl;
+  dest[3][1] = -(top + bottom) * tb;
+  dest[3][2] = (farZ + nearZ) * fn;
+  dest[3][3] = 1.0f;
+}
+
 void gf_commit_render_state() {
   gf_log(INFO_LOG, "Commiting render state.");
   if (render_state.viewport.dirty == true) {
     render_state.viewport.dirty = false;
     mat4 perspective_matrix;
-    glm_ortho(0.0f, render_state.viewport.width, 0,
-              render_state.viewport.height, -1.0, 1.0, perspective_matrix);
+    gf_ortho(0.0f, render_state.viewport.width, 0, render_state.viewport.height,
+             -1.0, 1.0, perspective_matrix);
     for (int i = 0; i < shader_program_list.count; i++) {
       struct shader *shader = &shader_program_list.shaders[i];
       gf_log(INFO_LOG, "Commiting render state to shader %i.", i);
