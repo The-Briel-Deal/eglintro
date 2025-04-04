5a4b5d9593135fbc1ccb36f08b739e1309287e33
Gabriel Ford <gabe@fordltc.net>
2025-04-03 20:02:02 -0400

    Create box equivalents to the triangle drawing functions.

--- include/draw.h
+++ include/draw.h
@@ -14,28 +14,32 @@ struct triangle_verts {
   struct vertex v2;
   struct vertex v3;
 };
-
 struct triangle_obj;
+
+struct box_verts {
+  struct vertex v1;
+  struct vertex v2;
+  struct vertex v3;
+  struct vertex v4;
+};
+struct box_obj;
+
 struct shader;
 
-//! Draw a box in the window, `x` is the percentage across the screen the top
-//! left of the box starts at. Width is the percentage size of the box relative
-//! to screen (e.g. 1.00 means the box is the width of the screen, and 0.50
-//! means the box's width is half the screen width. `y` and `height` follow the
-//! same principles.
-bool gf_draw_box(float x, float y, float width, float height);
+struct box_obj *gf_create_box(const struct box_verts *box_verts);
+bool gf_set_box_shader(struct box_obj *box, struct shader *shader);
+bool gf_draw_box(struct box_obj);
 
-struct shader *gf_compile_shaders(const char *vert_shader_src,
-                                  const char *frag_shader_src);
 
 //! Creates a opengl triangle object based off the triangle_verts passed.
 //! \param triangle_verts A pointer to the verticies the triangle will have.
 //! \return An opaque pointer to the resulting triangle object.
-struct triangle_obj *
-gf_create_triangle(const struct triangle_verts *triangle_verts);
-
-bool gf_set_triangle_shader(struct triangle_obj* triangle, struct shader* shader);
-
+struct triangle_obj *gf_create_triangle(const struct triangle_verts *triangle_verts);
+bool gf_set_triangle_shader(struct triangle_obj *triangle,
+                            struct shader *shader);
 bool gf_draw_triangle(struct triangle_obj *triangle);
+
+struct shader *gf_compile_shaders(const char *vert_shader_src,
+                                  const char *frag_shader_src);
 
 #endif
