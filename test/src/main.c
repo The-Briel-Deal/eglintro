478aa49aec19dc9270d25211f2aa898606e2bcf7
Gabriel Ford <gabe@fordltc.net>
2025-04-12 17:53:51 -0400

    Add a vec2s_scale test.

--- test/src/main.c
+++ test/src/main.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+
+#include "gf_math.h"
+#include <stdbool.h>
+
+#define RUN_TEST(test)                                                         \
+  do {                                                                         \
+    printf("Running Test " #test "\n");                                        \
+    test();                                                                    \
+  } while (false)
+
+void test_gf_vec2s_scale() {
+  vec2s v = {.x = 1.0, .y = 1.0};
+  gf_vec2s_scale(&v, 2.0);
+  assert(v.x == 2.0);
+  assert(v.y == 2.0);
+}
 
 int main() {
-	printf("Hello World");
+  RUN_TEST(test_gf_vec2s_scale);
 }
